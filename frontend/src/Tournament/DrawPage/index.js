b89786d2757e8a695054dd7dfcbc21e20be5263e
SteRoy <robinr045@gmail.com>
2021-01-17 16:59:37 +0000

    DrawPage add structure for teams displayed multiple times w/o sside processing

--- frontend/src/Tournament/DrawPage/index.js
+++ frontend/src/Tournament/DrawPage/index.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import {InputNumber} from "primereact/inputnumber";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import {Chip} from "primereact/chip";
 
 class DrawPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            motion: false,
+            scrolling: false,
+            scrollSpeed: 5,
+            draw: [
+                {highlight: "og", og: "Team A", oo: "Team B", cg: "Team C", co: "Team D", venue: "Room A", panel: "Matt Hazell, Jess Musulin"},
+                {highlight: "oo", og: "Team A", oo: "Team B", cg: "Team C", co: "Team D", venue: "Room A", panel: "Matt Hazell, Jess Musulin"},
+                {highlight: "cg", og: "Team A", oo: "Team B", cg: "Team C", co: "Team D", venue: "Room A", panel: "Matt Hazell, Jess Musulin"},
+                {highlight: "co", og: "Team A", oo: "Team B", cg: "Team C", co: "Team D", venue: "Room A", panel: "Matt Hazell, Jess Musulin"}
+            ].sort((a,b) => a[a.highlight] < b[b.highlight] ? -1 : 1)
+        }
+    }
+
     render() {
+        const teamTemplate = (column, rowData) => {
+            const key = column.header.toLowerCase();
+            return key === rowData.highlight ?
+                <Chip
+                    className="bg-primary text-white"
+                    label={rowData[key]}
+                    icon="pi pi-forward"
+                />
+                :
+                <span>{rowData[key]}</span>;
+        }
+
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <a className="navbar-brand" href="/">Tabbi3</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"/>
+                    <button
+                        className={`btn btn-outline-warning my-2 my-sm-0`}
+                        onClick={() => this.setState({motion: !this.state.motion})}
+                    >
+                        {this.state.motion ? "Draw" : "Motion"}
                     </button>
-
-                    <button className="btn btn-outline-warning my-2 my-sm-0">Motion</button>
                     <div className="nav-item p-mx-2 text-right">
                         <InputNumber
-                            value={5}
+                            value={this.state.scrollSpeed}
+                            onChange={(e) => this.setState({scrollSpeed: e.value})}
                             step={1}
                             min={1}
                             max={10}
@@ -27,8 +57,23 @@ class DrawPage extends React.Component {
                         />
                     </div>
 
-                    <button className="nav-item btn btn-outline-success p-mx-5">Start Scroll</button>
+                    <button
+                        className={`nav-item btn btn-outline-${this.state.scrolling ? "danger" : "success"} p-mx-5`}
+                        onClick={() => this.setState({scrolling: !this.state.scrolling})}
+                    >
+                        {this.state.scrolling ? "Stop" : "Start"} Scroll
+                    </button>
                 </nav>
+                <DataTable
+                    className="p-datatable-gridlines"
+                    value={this.state.draw}
+                >
+                    <Column field="venue" header="Venue"/>
+                    {["og", "oo", "cg", "co"].map(pos => (
+                        <Column body={(rowData, pos) => teamTemplate(pos, rowData)} header={pos.toUpperCase()}/>
+                    ))}
+                    <Column field="panel" header="Panel"/>
+                </DataTable>
             </div>
         )
     }

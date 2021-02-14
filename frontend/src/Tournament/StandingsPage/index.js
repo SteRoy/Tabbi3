819a2a6af870d6e13f7a099e26150b91b04b435e
SteRoy <stevenandrewroy@gmail.com>
2021-02-14 19:42:48 +0000

    Rank speaker scores in DESCENDING order.

--- frontend/src/Tournament/StandingsPage/index.js
+++ frontend/src/Tournament/StandingsPage/index.js
@@ -108,9 +108,9 @@ class StandingsPage extends React.Component {
                                 const t1sTotal = teamOne.speakTotal;
                                 const t2sTotal = teamTwo.speakTotal;
                                 if (t1sTotal < t2sTotal) {
-                                    return 1;
-                                } else if (t2sTotal < t1sTotal) {
                                     return -1;
+                                } else if (t2sTotal < t1sTotal) {
+                                    return 1;
                                 }
                             }
                             return teamOne.name < teamTwo.name ? -1 : 1;
@@ -141,9 +141,9 @@ class StandingsPage extends React.Component {
                     ]
                 }).flat().sort((speakerOne, speakerTwo) => {
                     if (speakerOne.totalSpeaks < speakerTwo.totalSpeaks) {
-                        return -1;
-                    } else if (speakerTwo.totalSpeaks < speakerOne.totalSpeaks) {
                         return 1;
+                    } else if (speakerTwo.totalSpeaks < speakerOne.totalSpeaks) {
+                        return -1;
                     } else {
                         return speakerOne.speakerName < speakerTwo.speakerName ? -1 : 1;
                     }

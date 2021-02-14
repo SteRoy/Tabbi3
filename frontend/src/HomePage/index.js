917083949266ee5db1f0c3fa58d1a54d4e5aaa9e
SteRoy <stevenandrewroy@gmail.com>
2021-02-14 13:53:38 +0000

    Add warning for individuals who haven't stored an institution.

--- frontend/src/HomePage/index.js
+++ frontend/src/HomePage/index.js
@@ -5,13 +5,32 @@ import TournamentCard from "../components/TournamentCard";
 import { Button } from 'primereact/button';
 
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {},
+            loggedIn: false
+        }
+    }
 
     render() {
         return (
             <div>
-                <NavBar active="home"/>
+                <NavBar active="home" userCB={(user, loggedIn) => this.setState({loggedIn, user})}/>
                 <div className="p-grid p-justify-center p-align-center p-mt-5">
                     <div className="p-col-11">
+                        {
+                            this.state.loggedIn ?
+                                this.state.user.Person.InstitutionMemberships.length === 0 ?
+                                    <a href={`/profile/me`}>
+                                        <div className="alert alert-danger">
+                                            <span className="pi pi-fw pi-exclamation-circle text-danger display-5" style={{'fontSize': '1.5em'}}/>
+                                            <span style={{'fontSize': '1.5em'}}> You have not set any current or previous institutions. Institutional clash will not apply to you. You can add an institution by clicking this alert.</span>
+                                        </div>
+                                    </a>
+                                    : ""
+                                : ""
+                        }
                         <Card>
                             <div className="display-4 text-center w-100">Current Tournaments</div>
                             <hr/>
